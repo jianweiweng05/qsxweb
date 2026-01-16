2c0c0ea0257d8b6bcd1dbf648473527efc20b7d9
Jianwei Weng <jianweiweng05.gmail.com>
2026-01-16 10:49:42 +0800

    128

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -33,7 +33,7 @@ export default function MainLayout({
 
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex-1">{children}</div>
+      <div className="flex-1 flex justify-center">{children}</div>
       <nav className="sticky bottom-0 border-t border-white/10 bg-black/90 backdrop-blur-sm" data-nav="LAYOUT_MAIN_V2">
         <div className="flex justify-around items-center h-14">
           {tabs.map((tab) => {
